213fec3a241f825ca9c597155128272c72a13768
cykable <36812758+cykable@users.noreply.github.com>
2018-05-29 18:23:03 +0300

    invinte

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ robot.on('message', message => {
     if(message.content.startsWith(p + 'help')) {
         const embed = new Discord.RichEmbed()
             .setTitle("Помощь")
-            .setColor("#00BFFF")
-            .setDescription('Мои команды:\n ○ h!help-пмощь по командам \n ○ h!say-сказать от имени бота.')
+            .setColorobotr("#00BFFF")
+            .setDescription('Мои команды:\n ○ h!help-пмощь по командам \n ○ h!say-сказать от имени бота. \n ○ h!inv-Приглосить бота себе на сервер ')
+            .setFooter("HEE4")
+            .setTimestamp();
+        message.channel.send({embed});
+    }
+});
+
+robot.on('message', message => {
+    if(message.content.startsWith(p + `inv`)) {
+        const embed = new Discord.RichEmbed()
+            .setTitle("Ссылка на бота")
+            .setColorobotr("#00BFFF")
+            .setDescription('\n Приглоси себе бота на сервер. \n https://discordapp.com/oauth2/authorize?client_id=444545508963385364&scope=bot&permissions=1723325513')
             .setFooter("HEE4")
             .setTimestamp();
         message.channel.send({embed});
@@ -39,7 +51,7 @@ function status2() {
 
 function status2() {
     robot.user.setActivity('h!help', { type: "PLAYING" })
-    setTimeout(status1, 10000)
+    setTimeout(status1, 20000)
 }
 
 robot.login(process.env.SECRET);
