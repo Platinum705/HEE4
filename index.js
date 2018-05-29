bc39728dcd4d465cc90e45e3f5f4f2ddae9829d1
cykable <36812758+cykable@users.noreply.github.com>
2018-05-29 18:28:17 +0300

    delet invinte

--- index.js
+++ index.js
@@ -19,17 +19,7 @@ robot.on('message', message => {
     }
 });
 
-robot.on('message', message => {
-    if(message.content.startsWith(p + `inv`)) {
-        const embed = new Discord.RichEmbed()
-            .setTitle("Ссылка на бота")
-            .setColorobotr("#00BFFF")
-            .setDescription('\n Приглоси себе бота на сервер. \n https://discordapp.com/oauth2/authorize?client_id=444545508963385364&scope=bot&permissions=1723325513')
-            .setFooter("HEE4")
-            .setTimestamp();
-        message.channel.send({embed});
-    }
-});
+
 
 robot.on('message', message => {
     if(message.content.startsWith(p + 'say')) {
