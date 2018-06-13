8f8a5f8a494badfb5cba8104d3ee25097a0ade87
cykable <36812758+cykable@users.noreply.github.com>
2018-06-13 10:20:53 +0300

    adorned h!help

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ robot.on('message', message => {
         const embed = new Discord.RichEmbed()
             .setTitle("Помощь")
             .setColor("#00BFFF")
-            .setDescription('Мои команды:\n ○ h!help-пмощь по командам. \n ○ h!say-сказать от имени бота. \n ○ h!inv-Приглосить бота себе на сервер. \n ○ h!poll-устроить голосование. \n ○ h!afk on-войти в AFK. \n ○ h!afk off-выйти из AFK.')
+            .setDescription('Мои команды:\n ○ ***h!help***-пмощь по командам. \n ○ ***h!say***-сказать от имени бота. \n ○ ***h!inv***-приглосить бота себе на сервер. \n ○ ***h!poll***-устроить голосование. \n ○ ***h!afk on***-войти в AFK. \n ○ ***h!afk off***-выйти из AFK.')
             .setFooter("HEE4")
             .setTimestamp();
         message.channel.send({embed}).then(sentMessage => {   
@@ -64,7 +64,6 @@ robot.on('message', message => {
 robot.on('message', message => {
     if(message.content.startsWith(p + 'say')) {
         let say = message.content.slice((p + 'say').length);
-        message.delete();
         message.channel.send(say);
     }
 });
